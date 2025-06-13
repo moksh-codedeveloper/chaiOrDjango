2ea692d2917a3d30e663e94ebf01b1dc458eceae
moksh-codedeveloper <maldemoksh437@gmail.com>
2025-06-13 12:37:28 +0530

    Remove obsolete build script for project setup

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-pip install -r requirements.txt
-pip install --upgrade pip
-
-
-# Run Django setup
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
